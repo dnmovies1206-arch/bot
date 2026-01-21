88557d329b6c574808da5d2e6da8c4fde7eba2d0
dnmovies1206-arch <dnmovies1206@gmail.com>
2026-01-21 09:28:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,8 +61,8 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-1003620626719")# Channels for
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/e419f801841c2ee3db0fc.jpg')    # QR code image for payments
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
+QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/5gQV9fNV/photo-2026-01-21-03-57-26-7597659080605302800.jpg')    # QR code image for payments
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'karthik93gm@oksbi')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
     10: "7day",
