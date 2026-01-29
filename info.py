0591a5af91fea8998533d5d54081c2f0badd1ce7
dnmovies1206-arch <dnmovies1206@gmail.com>
2026-01-29 09:19:47 +0530

    Fix formatting of auth_req_channels and auth_channels

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003469200638')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002988640125")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1002988640125")# Channels for force sub (make sure bot is admin)
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002988640125 ")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_channels     = environ.get("AUTH_CHANNELS", "-1002988640125 ")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
