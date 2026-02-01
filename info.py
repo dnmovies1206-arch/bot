b9ea5ae4bbd75cb54202bc72d2c5fd151f7c1628
dnmovies1206-arch <dnmovies1206@gmail.com>
2026-02-01 18:40:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,7 +100,7 @@ LANDSCAPE_POSTER = bool(environ.get('LANDSCAPE_POSTER', True)) # Shows landscape
 IS_VERIFY = is_enabled('IS_VERIFY', True)  # Verification On (True) / Off (False)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1003434477634')) #Verification Channel Id 
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1003434477634')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://files.catbox.moe/cb0inw.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/How_To_Open_the_Link_01")   # Tutorial link for verification
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/How_To_Open_the_Link_01")   # Second tutorial link for verification
