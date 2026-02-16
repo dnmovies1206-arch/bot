4b35c498abbcffbb1e6e63e785cc16c93e1662da
dnmovies1206-arch <dnmovies1206@gmail.com>
2026-02-16 16:29:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "60"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/dragonkannadavmovie') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/dragonkannadavmovie https://t.me/dragonmoviesgroup') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+GB4qVgc43bE2MWY9') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+GB4qVgc43bE2MWY9') # Update channel link for the bot
 
